8dab89cf50a6cd440887397ead24774bb7d041f5
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-08 19:31:38 +0200

    sa suffit la

--- server.js
+++ server.js
@@ -20,117 +20,95 @@ const db = new Client({
 // 	connectionString: 'postgresql://sunlight_admin-univership:uni$$123@postgresql-sunlight.alwaysdata.net/sunlight_univership'
 // });
 
-
 db.connect();
 
-// Préparation des queries
+
+
+// fonction permettant de voir tout les articles
+async function run(query) {
+	let result = await db.query(query);
+	return result;
+}
+
+
+
+
+
+
 const queryView = {
 	text: "SELECT * FROM article"
 }
 
+// Routes
 
-
-
-
-// fonction permettant de lancer une requête sans retour
-function run(query) {
-	db.query(query, (err, res)=>{
-		if (err) {
-			console.log(err);
-		}
-	});
-}
-
-
-
-
-// fonction permettant de regarder la requete
-function view(query) {
-	let result = '';
-	db.query(query, (err, res)=>{
-		if (err) {
-			console.log(err);
-			result = err
-		}
-		else {
-			console.log(res.rows)
-			result = res.rows
-		}
-	})
-	.then(()=>{
-		return result
-	})
-}
-
-
-
-
-
-
-app.get('/', (req, res) =>{
-	const result = view(queryView);
-	res.json(result);
+app.get('/', async (req, res) =>{
+	const result = await run(queryView);
+	res.json(result.rows);
 })
 
 
 
 // Récupérer la liste complète des articles
-app.get('/list', (req, res) =>{
-	const result = view(queryView);
-	res.json(result);
+app.get('/list', async (req, res) =>{
+	const result = await run(queryView);
+	res.json(result.rows);
 })
 
 
 //Récupérer un article spécifique
-app.get('/article/:id', (req, res) =>{
+app.get('/article/:id', async (req, res) =>{
 	let index = Number(req.params.id);
 	let querySolo = {
 		text: "SELECT * FROM article WHERE id=$1",
 		values: [index]
 	};
-	const result = run(querySolo);
-	res.json(result);
+	const result = await run(querySolo);
+	res.json(result.rows[0]);
 
 })
 
 
 //création d'un article
-app.post('/create', (req, res)=> {
+app.post('/create', async (req, res)=> {
 	const corps = req.body;
 	let queryInsert = {
 		text: "INSERT INTO article (title, category, content, date, img, visibility) VALUES($1,$2,$3,$4,$5,$6)",
-		values: [corps.title, corps.category, corps.content, date, corps.img, corps.visibility]
+		values: [corps.title, corps.category, corps.content, corps.date, corps.img, corps.visibility]
 	}
-	const result = run(queryInsert);
-	res.send(result);
+	run(queryInsert);
+	res.send('contenu crée');
 });
 
 
 //Modification d'un article
 app.post('/edit/:id', (req, res)=> {
-	const index = Number(req.params.id)+1;
+	const index = Number(req.params.id);
 	const corps = req.body;
 	let queryUpdate = {
 		text: "UPDATE article SET title = $1, category = $2, content = $3, date = $4, img = $5, visibility = $6 WHERE id = $7",
 		values: [corps.title, corps.category, corps.content, corps.date, corps.img, corps.visibility, index]
 	}
 	// Mettre à jour la ligne lié à l'article sélectionné avec son id
-	const result = run(queryUpdate);
-	res.send(result);
+	run(queryUpdate);
+	res.send('contenu mis à jour');
 });
 
 
 //Suppression d'un article
 app.delete('/delete/:id', (req, res)=> {
-	const index = Number(req.params.id)+1;
+	const index = Number(req.params.id);
 
 	let queryDelete = {
-		text: "DELETE FROM article WHERE id = ?",
+		text: "DELETE FROM article WHERE id = $1",
 		values: [index]
 	}
-	const result = run(queryDelete);
-	res.send(result);
+	run(queryDelete)
+	.catch(err=>console.log(err))
+	res.send('contenu supprimé');
 });
 
 
 app.listen(process.env.PORT, ()=> console.log('Server started! 🎉'));
+
+
+
